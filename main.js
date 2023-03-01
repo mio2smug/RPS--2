1f1996b07c8e938ba0fb354f610714317dc651df
mio2smug <bryan.lazarowork@yahoo.com>
2023-03-01 13:17:27 -0500

    added some test JS

---
+++ main.js
@@ -0,0 +1,12 @@
+// console.log("Testing 1, 2, working")
+
+
+//  HTML Constants
+const testButton = document.getElementById('test');
+const myGameboard = document.getElementById('myGameboard')
+const myClickables = myGameboard.querySelectorAll('div')
+
+
+//functions
+testButton.onclick = () => console.log(myClickables); // working
+
